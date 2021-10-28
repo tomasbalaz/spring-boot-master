356aab3d742293a78631979d93b29e1075e60452
Tomas Balaz <balaztomas0@gmail.com>
2021-10-28 14:38:17 +0200

    Add different implementation of CustomerRepository and CustomerFakeRepository

--- src/main/java/sk/balaz/springbootmaster/customer/CustomerFakeRepository.java
+++ src/main/java/sk/balaz/springbootmaster/customer/CustomerFakeRepository.java
@@ -10,9 +10,16 @@ public class CustomerFakeRepository implements CustomerRepo {
 
     @Override
     public List<Customer> getCustomers() {
-        return Arrays.asList(
+        // 1. solution not recommended
+//        return Arrays.asList(
+//                new Customer(1L, "James Bond"),
+//                new Customer(2L, "Jamila Conc")
+//                );
+
+        // 2. solution supported from Java 9
+        return List.of(
                 new Customer(1L, "James Bond"),
                 new Customer(2L, "Jamila Conc")
-                );
+        );
     }
 }
--- src/main/java/sk/balaz/springbootmaster/customer/CustomerRepository.java
+++ src/main/java/sk/balaz/springbootmaster/customer/CustomerRepository.java
@@ -1,15 +1,30 @@
 package sk.balaz.springbootmaster.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Component
+@Primary
 public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
         //TODO connect to real DB
-        return Collections.emptyList();
+
+        // 1. solution supported before Java 9
+        //return Collections.emptyList();
+
+        // 2. solution supported before Java 9
+//        return Collections.singletonList(
+//                new Customer(1L, "TODO Implement real DB")
+//        );
+
+        // 3. solution supported after Java 9
+        return List.of(
+                new Customer(1L, "TODO Implement real DB")
+        );
     }
 }
