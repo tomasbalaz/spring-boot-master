e246e737c6971b520b9948baf8e7d64683589300
Tomas Balaz <balaztomas0@gmail.com>
2021-11-02 15:14:19 +0100

    Add POST, PUT, DELETE REST methods

--- src/main/java/sk/balaz/springbootmaster/customer/CustomerController.java
+++ src/main/java/sk/balaz/springbootmaster/customer/CustomerController.java
@@ -1,8 +1,7 @@
 package sk.balaz.springbootmaster.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +13,23 @@ public class CustomerController {
     @Autowired
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    @PostMapping
+    void createNewCustomer(@RequestBody Customer customer) {
+        System.out.println("POST REQUEST...");
+        System.out.println(customer);
+    }
+
+    @PutMapping
+    void updateCustomer(@RequestBody Customer customer) {
+        System.out.println("UPDATE REQUEST...");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    void deleteCustomer(@PathVariable("customerId") Long id) {
+        System.out.println("DELETE REQUEST FOR CUSTOMER WITH ID " + id);
     }
 
     @GetMapping
