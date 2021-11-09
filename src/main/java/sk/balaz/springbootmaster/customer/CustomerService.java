e847175579ef62e0c6c36af60cffae5f0e32d522
Tomas Balaz <balaztomas0@gmail.com>
2021-11-09 10:27:39 +0100

    Create NotFoundException mapped as HttpStatus.NOT_FOUND

--- src/main/java/sk/balaz/springbootmaster/customer/CustomerService.java
+++ src/main/java/sk/balaz/springbootmaster/customer/CustomerService.java
@@ -2,6 +2,7 @@ package sk.balaz.springbootmaster.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sk.balaz.springbootmaster.exception.NotFoundException;
 
 import java.util.List;
 
@@ -24,7 +25,9 @@ public class CustomerService {
                 .stream()
                 .filter(customer -> customer.getId().equals(id))
                 .findFirst()
-                .orElseThrow(() -> new IllegalStateException("customer not foud"));
+                .orElseThrow(
+                        () -> new NotFoundException(
+                                "customer with id '" + id + "' not found"));
 
     }
 }
---
+++ src/main/java/sk/balaz/springbootmaster/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package sk.balaz.springbootmaster.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
