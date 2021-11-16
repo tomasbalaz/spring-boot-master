112bfb4af5a0569f0936624b1995365482c31cc7
Tomas Balaz <balaztomas0@gmail.com>
2021-11-16 13:48:36 +0100

    Introduce to spring boot annotation @ConfigurationProperties

--- src/main/java/sk/balaz/springbootmaster/customer/CustomerConfig.java
+++ src/main/java/sk/balaz/springbootmaster/customer/CustomerConfig.java
@@ -1,9 +1,14 @@
 package sk.balaz.springbootmaster.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import sk.balaz.springbootmaster.infoapp.InfoApp;
+
+import java.util.Arrays;
 
 @Configuration
 public class CustomerConfig {
@@ -12,9 +17,23 @@ public class CustomerConfig {
     @Value("${app.useCustomerRepo:false}")
     private Boolean useCustomerRepo;
 
-   @Bean
-    CommandLineRunner commandLineRunner() {
-        return  args -> System.out.println("Command line runner");
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
+        return args -> {
+
+            System.out.println("Command line runner");
+
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
+            System.out.println(Arrays.toString(environment.getActiveProfiles()));
+            System.out.println(infoApp);
+        };
     }
 
     @Bean
